c9dbeec03a1a73aa1a26813819109ae70877512b
Himani yadav <73442402+himaniyadav18@users.noreply.github.com>
2023-05-23 22:48:42 +0530

    Create abc.js

---
+++ pages/abc.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from "react";
+import Multiselect from "multiselect-react-dropdown";
+
+
+function abc({arr}) {
+  return(
+    <div>
+      console.log({arr})
+      <h1>hello</h1>
+    </div>
+  ) 
+}
+
+export default abc
+
