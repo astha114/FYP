77abb6532fc9222255ea35cc4edef385f9a46cde
astha <72337639+astha114@users.noreply.github.com>
2023-05-23 22:04:55 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains:  ['ipfs.infura.io', 'th.bing.com', 'fyphms.infura-ipfs.io']
+   }
+}
+module.exports = nextConfig
